1a80c7e1b4f5e648c61cf016b8eb37b6091cc597
MichaelGenchev <89229268+MichaelGenchev@users.noreply.github.com>
2022-07-19 16:43:10 +0300

    Add files via upload

---
+++ problem3/validWords.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"regexp"
+	"strings"
+)
+
+
+func main() {
+
+	sentence := "cat and  -dog!"
+
+	// sentence := "!this  1-s b8d!"
+
+	result := countValidWords(sentence)
+
+	fmt.Println(result)
+
+}
+
+func countValidWords(sentence string) int {
+
+	arrayWords := strings.Fields(sentence)
+
+	validWords := filterWords(arrayWords)
+
+	return len(validWords)
+}
+
+
+func filterWords(words []string) []string {
+	validwords := []string{}
+	for _, word := range words{
+
+		matched, err := regexp.MatchString(`^([a-z]+(-?[a-z]+)?)?(!|\\.|,)?$`, word)
+		fmt.Println(matched)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if matched == true{
+			validwords = append(validwords, word)
+		}
+
+	}
+
+	return validwords
+}
